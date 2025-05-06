9dcffc2a41c79eb3eb877e1f70cf2def547922ba
KevDeto <contactohuayta@gmail.com>
2025-05-06 14:32:38 -0300

    agregue un banner de bienvenida y un banner de informacion

---
+++ script.js
@@ -0,0 +1,60 @@
+const swiper = new Swiper('.swiper-descuentos', {
+    // Optional parameters
+    slidesPerview: 1,
+    direction: 'horizontal',
+    spaceBetween: 20,
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+
+
+/*
+let infoSwiper;
+
+function initSwiper() {
+  if (window.innerWidth < 992) { // Ajusta el breakpoint según necesites
+const swiper2 = infoSwiper = new Swiper('.swiper-informacion', {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    }
+  }
+});
+  }
+}
+
+// Inicializar al cargar
+document.addEventListener('DOMContentLoaded', initSwiper);
+
+// Revisar al cambiar tamaño
+window.addEventListener('resize', function() {
+  if (window.innerWidth >= 992 && infoSwiper) {
+    infoSwiper.destroy();
+    infoSwiper = undefined;
+  } else if (window.innerWidth < 992 && !infoSwiper) {
+    initSwiper();
+  }
+});
+*/
